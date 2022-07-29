56e9cfc24ea6187c42a55399e4dd791a25f1cf81
Leonardo Cordeiro <leonardo.cordeiro@produtec.com.br>
2022-07-29 15:52:36 -0300

    Clonando objetos com spread

---
+++ 05-06-clonando-objetos-com-spread/index.js
@@ -0,0 +1,12 @@
+const user = {
+    name: 'danielbonifacio',
+    email: 'contato@danielbonificio.com.br'
+}
+
+const newUser = {...user}
+
+// newUser.email = 'joao@zinho.com'
+// newUser.name - 'joaozinho123'
+
+console.log(user)
+console.log(newUser)
