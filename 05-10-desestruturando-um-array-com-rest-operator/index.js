4fce412aa9a7fb52c38651f76d8778be14450418
José Leonardo Cordeiro Bahia <leonardo_cordeirobahia@hotmail.com>
2022-07-31 10:55:02 -0300

    Desestruturando um array com Rest operator

---
+++ 05-10-desestruturando-um-array-com-rest-operator/index.js
@@ -0,0 +1,6 @@
+const fruits = ['banana', 'morango', 'manga', 'kiwi', 'maracujá']
+const [firstItem, secondItem, ...otherItems] = fruits
+
+console.log(firstItem)
+console.log(secondItem)
+console.log(otherItems)
